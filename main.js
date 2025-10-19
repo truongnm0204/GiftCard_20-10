ad099b415684da9ffd380e192f973b99f68ae236
Truong Nguyen Manh <truongnguyenmanh411@gmail.com>
2025-10-19 16:28:18 +0700

    before fix

---
+++ main.js
@@ -0,0 +1,15 @@
+onload = () => {
+        document.body.classList.remove("container");
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+        var audioElement = document.getElementById("myAudio");
+
+        // Bắt đầu phát từ giây thứ 10
+        audioElement.currentTime = 8;
+
+        // Chơi bài nhạc
+        audioElement.play().catch(function(error) {
+                console.error("Không thể phát âm thanh:", error);
+            });
+});
